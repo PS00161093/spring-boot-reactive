e289c7636bb2f0eb60cf93721031999d9cec84ea
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-01-11 22:52:01 +0530

    updated project code

--- src/main/java/com/learn/reactivespringboot/dao/ChapterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learn.reactivespringboot.dao;
-
-import com.learn.reactivespringboot.domain.Chapter;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface ChapterRepository extends ReactiveCrudRepository<Chapter, String> {
-
-
-}
--- src/main/java/com/learn/reactivespringboot/service/ChapterService.java
+++
@@ -1,9 +0,0 @@
-package com.learn.reactivespringboot.service;
-
-import com.learn.reactivespringboot.domain.Chapter;
-import reactor.core.publisher.Flux;
-
-public interface ChapterService {
-
-    Flux<Chapter> getAllChapters();
-}
