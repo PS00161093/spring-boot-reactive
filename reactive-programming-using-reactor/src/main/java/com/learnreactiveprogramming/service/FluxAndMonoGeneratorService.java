d7c98eda3740c2086a26c314f3cd30cead240149
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-01-11 23:55:29 +0530

    Creating names Mono using Mono.just()

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,6 +1,7 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -10,11 +11,19 @@ public class FluxAndMonoGeneratorService {
         return Flux.fromIterable(List.of("Alex", "Ben", "Chloe"));
     }
 
+    public Mono<String> nameMono() {
+        return Mono.just("Alex");
+    }
+
     public static void main(String[] args) {
 
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
         fluxAndMonoGeneratorService
                 .namesFlux()
+                .subscribe(name -> System.out.println("Name is : " + name));
+
+        fluxAndMonoGeneratorService
+                .nameMono()
                 .subscribe(name -> System.out.println("Name is : " + name));
     }
 }
