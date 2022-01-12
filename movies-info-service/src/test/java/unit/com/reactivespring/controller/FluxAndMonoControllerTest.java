b13457342197d1646c7fbb5026b1116ebe7142c0
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-01-13 00:13:56 +0530

    Using returnResult() & expectBodyList() to API verify response data.

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @WebFluxTest(controllers = FluxAndMonoController.class)
 @AutoConfigureWebTestClient
@@ -23,6 +27,45 @@ class FluxAndMonoControllerTest {
                 .is2xxSuccessful()
                 .expectBodyList(Integer.class)
                 .hasSize(3);
+    }
+
+    @Test
+    void fluxResponse() {
+        var fluxResponse = webTestClient
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier
+                .create(fluxResponse)
+                .expectNext(1)
+                .expectNext(2)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxResponseWithConsume() {
+        webTestClient
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .consumeWith(fluxResponse -> {
+                    var response = fluxResponse.getResponseBody();
+                    assertNotNull(response);
+                    assertEquals(3, response.size());
+                    assertEquals(1, response.get(0));
+                    assertEquals(2, response.get(1));
+                    assertEquals(3, response.get(2));
+                });
+
     }
 
 }
