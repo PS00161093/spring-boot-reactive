fbb0b6b4efbaa9080e7fdb9917c755e12caf5622
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-01-15 18:57:47 +0530

    Added addReview functionality

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -1,4 +1,27 @@
 package com.reactivespring.handler;
 
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
 public class ReviewHandler {
+
+    private ReviewReactiveRepository reviewReactiveRepository;
+
+    public ReviewHandler(ReviewReactiveRepository reviewReactiveRepository) {
+        this.reviewReactiveRepository = reviewReactiveRepository;
+    }
+
+    public Mono<ServerResponse> addReview(ServerRequest serverRequest) {
+        return serverRequest
+                .bodyToMono(Review.class)
+                .flatMap(review -> reviewReactiveRepository.save(review))
+                .flatMap(savedReview -> ServerResponse.status(HttpStatus.CREATED).bodyValue(savedReview))
+                .log();
+    }
 }
--- movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
+++ movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
@@ -1,5 +1,6 @@
 package com.reactivespring.router;
 
+import com.reactivespring.handler.ReviewHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -10,9 +11,10 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 public class ReviewRouter {
 
     @Bean
-    public RouterFunction<ServerResponse> reviewRoute() {
+    public RouterFunction<ServerResponse> reviewRoute(ReviewHandler reviewHandler) {
         return RouterFunctions.route()
                 .GET("/v1/helloworld", req -> ServerResponse.ok().bodyValue("helloworld"))
+                .POST(("/v1/reviews"), req -> reviewHandler.addReview(req))
                 .build();
     }
 }
