c7511a63ceb0180a0e4351ed127a006e847eccd6
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-01-15 22:58:10 +0530

    handled 4xx response from  movies-info service

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -1,7 +1,9 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.exception.MoviesInfoClientException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -24,6 +26,17 @@ public class MoviesInfoRestClient {
                 .get()
                 .uri(url, movieId)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, moviesInfoResponse -> {
+                    if (moviesInfoResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                        return Mono.error(
+                                new MoviesInfoClientException("No Movie available for passed movieId :" + movieId,
+                                        moviesInfoResponse.statusCode().value()));
+                    }
+                    return moviesInfoResponse.bodyToMono(String.class)
+                            .flatMap(responseMsg -> Mono.error(
+                                    new MoviesInfoClientException(responseMsg, moviesInfoResponse.statusCode().value())
+                            ));
+                })
                 .bodyToMono(MovieInfo.class)
                 .log();
     }
