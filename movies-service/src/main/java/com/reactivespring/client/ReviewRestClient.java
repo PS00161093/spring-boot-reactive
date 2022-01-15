c0e838434c4a3b7bb696a1e34f7ddcf9c5527623
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-01-15 23:27:22 +0530

    handled 4xx & 5xx response from  movies-review service

--- movies-service/src/main/java/com/reactivespring/client/ReviewRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/ReviewRestClient.java
@@ -1,11 +1,16 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.Review;
+import com.reactivespring.exception.ReviewsClientException;
+import com.reactivespring.exception.ReviewsServerException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
 public class ReviewRestClient {
@@ -30,8 +35,25 @@ public class ReviewRestClient {
                 .get()
                 .uri(url)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, reviewsResponse -> handle4xxError(movieId, reviewsResponse))
+                .onStatus(HttpStatus::is5xxServerError, reviewsResponse -> handle5xxError(movieId, reviewsResponse))
                 .bodyToFlux(Review.class)
                 .log();
+    }
+
+    private Mono<Throwable> handle5xxError(String movieId, ClientResponse reviewsResponse) {
+        return reviewsResponse.bodyToMono(String.class)
+                .flatMap(responseMsg -> Mono.error(
+                        new ReviewsServerException("Server exception in ReviewService : " + responseMsg)
+                ));
+    }
+
+    private Mono<Throwable> handle4xxError(String movieId, ClientResponse reviewsResponse) {
+        if (reviewsResponse.statusCode().equals(HttpStatus.NOT_FOUND)) return Mono.empty();
+        return reviewsResponse.bodyToMono(String.class)
+                .flatMap(responseMsg -> Mono.error(
+                        new ReviewsClientException(responseMsg)
+                ));
     }
 
 }
